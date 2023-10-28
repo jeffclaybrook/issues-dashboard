e40b9ee89f1e430de5a1ac6eaa3b5cd9b78b3a9a
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2023-10-28 13:27:13 -0500

    Add files via upload

---
+++ issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@radix-ui/themes"
+import { Pencil2Icon } from "@radix-ui/react-icons"
+import Link from "next/link"
+
+const EditIssueButton = ({ issueId }: { issueId: number }) => {
+ return (
+  <Button>
+   <Pencil2Icon />
+   <Link href={`/issues/edit/${issueId}`}>Edit Issue</Link>
+  </Button>
+ )
+}
+
+export default EditIssueButton
